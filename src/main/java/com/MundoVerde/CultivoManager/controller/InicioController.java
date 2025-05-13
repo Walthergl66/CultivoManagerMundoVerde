6716d334567a1cebb81388aa6719db791516dc74
Walthergl66 <waltherjr68@gmail.com>
2025-05-13 08:35:26 -0500

    1

--- src/main/java/com/MundoVerde/CultivoManager/controller/InicioController.java
+++ src/main/java/com/MundoVerde/CultivoManager/controller/InicioController.java
@@ -2,14 +2,14 @@ package com.MundoVerde.CultivoManager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class InicioController {
 
     @GetMapping("/")
-    @ResponseBody
+    // @ResponseBody
     public String inicio() {
-        return "¡Bienvenido a CultivoManager!";
+        return "inicio"; // Carga el archivo inicio.html desde templates
     }
 }
