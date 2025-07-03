ca455b731485d23dbb6293e1b02699250212a7a1
Walthergl09 <waltherjr68@gmail.com>
2025-07-03 17:08:34 -0500

    Aplique el Authorization con Swagger

--- src/main/java/com/MundoVerde/CultivoManager/config/SwaggerConfig.java
+++ src/main/java/com/MundoVerde/CultivoManager/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.MundoVerde.CultivoManager.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
 
     @Bean
     public OpenAPI apiInfo() {
@@ -14,6 +19,14 @@ public class SwaggerConfig {
             .info(new Info()
                 .title("Cultivo Manager API")
                 .description("Documentación de la API de Mundo Verde")
-                .version("1.0"));
+                .version("1.0"))
+            .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+            .components(new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                    new SecurityScheme()
+                        .name("Authorization") 
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .description("Ingresa solo el token JWT: Bearer <token>")));
     }
 }
